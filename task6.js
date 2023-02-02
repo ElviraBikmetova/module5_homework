9c8e946b20aa452314fdd293e9e9c8d15c290aaa
Elvira <gaellru@gmail.com>
2023-02-02 13:22:30 +0300

    Update task6.js

--- task6.js
+++ task6.js
@@ -1,14 +1,14 @@
 // Задание 6. Дан массив. Проверить, одинаковые ли элементы в массиве и вывести результат true или false в консоль. Речь идёт не о двух рядом стоящих одинаковых элементах, а обо всех. Проверить, все ли элементы в массиве одинаковые.
 
-let commonArr = [13, 13, 20, 45, 66, 20, 18, 15, 20];
+let commonArr = [13, 13, 20, 45, 66, 20, 18, 15, 13];
 let commonArr2 = [13, 13, 13];
 let commonArr3 = [13, 13, '13'];
 let commonArr4 = ['yes', 'no', '13'];
 let commonArr5 = ['yes', 'yes', 'yes'];
 
-// 1 вариант 
+// 1 вариант
 let check;
-for (let i = 0; i < commonArr5.length; i++) {
+for (let i = 0; i < commonArr.length; i++) {
   if (commonArr[i] == commonArr[0]) {
     check = true;
   } else {
